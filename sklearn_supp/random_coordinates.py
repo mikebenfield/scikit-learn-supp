0b6dc0c66a92ebe53a2975e184dbc314364cf2f0
Michael Benfield <mike.benfield@gmail.com>
2017-01-23 15:01:29 -0800

    Formatting / PEP8.

--- sklearn_supp/random_coordinates.py
+++ sklearn_supp/random_coordinates.py
@@ -1,13 +1,13 @@
 import numpy as np
-from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin, clone
+from sklearn import ensemble, tree
+from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
 from sklearn.utils import check_random_state
-from sklearn import metrics, ensemble, tree, linear_model
-from sklearn.model_selection import train_test_split
 
 
 def random_point_on_sphere(dim=2, random_state=None):
     """
-    Return a random point on the sphere, uniformly selected with respect to volume.
+    Return a random point on the sphere, uniformly selected with respect to
+    volume.
     """
     random_state = check_random_state(random_state)
     pt = 0
@@ -22,15 +22,15 @@ def random_son(dim=2, random_state=None):
     """
     Return a random element of SO(dim).
 
-    SO(dim) in this case consists of the orthogonal matrices with determinant 1.
-    Matrices are selected uniformly with respect to Haar measure.
+    SO(dim) in this case consists of the orthogonal matrices with determinant
+    1. Matrices are selected uniformly with respect to Haar measure.
     """
     random_state = check_random_state(random_state)
     result = -np.eye(dim, dtype=np.float)
 
     result[0, 0] = 1
 
-    for i in range(2, dim+1):
+    for i in range(2, dim + 1):
         x = random_point_on_sphere(dim=i, random_state=random_state)
         result[:i, :i] = \
             result[:i, :i] - (np.dot(2*x, result[:i, :i])[:, np.newaxis]*x).T
@@ -41,7 +41,7 @@ def random_son(dim=2, random_state=None):
 class RandomCoordinateChanger(BaseEstimator, TransformerMixin):
     """
     Choose a random change of coordinates and apply to X.
-    
+
     Parameters:
 
     usecols - a sequence of integers indicating the columns of X on which
@@ -53,22 +53,27 @@ class RandomCoordinateChanger(BaseEstimator, TransformerMixin):
     change. If None or greater than len(usecols), the dimension of the
     coordinate change will be len(usecols). The only reason for this to be
     less than len(usecols) is performance.
-    
+
     transform_repeats - the number of random coordinate changes to select
     and apply. If transform_dimension is len(usecols), there is no reason
     for this to be greater than 1. If transform_repeats is 'auto',
     transform_dimension // len(usecols) will be used.
-    
+
     copy - whether to make a copy of X (otherwise it will be transformed in
     place).
-    
+
     random_state - int, RandomState instance or None, optional (default=None)
     If int, random_state is the seed used by the random number generator. If
     RandomState instance, random_state is the random number generator. If None,
     the random number generator is the RandomState instance used by np.random.
     """
-    def __init__(self, transform_dimension=None, transform_repeats='auto', copy=True,
-                 usecols=None, random_state=None):
+
+    def __init__(self,
+                 transform_dimension=None,
+                 transform_repeats='auto',
+                 copy=True,
+                 usecols=None,
+                 random_state=None):
         self.transform_dimension = transform_dimension
         self.transform_repeats = transform_repeats
         self.copy = copy
@@ -95,17 +100,18 @@ class RandomCoordinateChanger(BaseEstimator, TransformerMixin):
             self.real_repeats_ = self.transform_repeats
 
         def create_son():
-            return random_son(dim=self.real_dimension_,
-                              random_state=self.random_state)
-        self.transforms = np.array([create_son()
-                                    for i in range(self.real_repeats_)])
+            return random_son(
+                dim=self.real_dimension_, random_state=self.random_state)
+
+        self.transforms = np.array(
+            [create_son() for i in range(self.real_repeats_)])
 
         def choose_indices():
-            return self.random_state.choice(indices,
-                                            size=self.real_dimension_,
-                                            replace=False)
-        self.indices_ = np.array([choose_indices()
-                                 for i in range(self.real_repeats_)])
+            return self.random_state.choice(
+                indices, size=self.real_dimension_, replace=False)
+
+        self.indices_ = np.array(
+            [choose_indices() for i in range(self.real_repeats_)])
         return self
 
     def transform(self, X):
@@ -123,6 +129,7 @@ class CoordinateChangingDecisionTree(BaseEstimator):
     """
     Don't use this directly.
     """
+
     def __init__(self,
                  max_depth=None,
                  min_samples_split=2,
@@ -168,16 +175,15 @@ class CoordinateChangingDecisionTree(BaseEstimator):
             min_weight_fraction_leaf=self.min_weight_fraction_leaf,
             splitter=self.splitter,
             presort=self.presort,
-            max_leaf_nodes = self.max_leaf_nodes,
-            min_impurity_split = self.min_impurity_split,
+            max_leaf_nodes=self.max_leaf_nodes,
+            min_impurity_split=self.min_impurity_split,
             random_state=self.random_state)
 
         self.transformer_ = RandomCoordinateChanger(
             transform_dimension=self.transform_dimension,
             transform_repeats=self.transform_repeats,
             usecols=self.usecols,
-            random_state=self.random_state,
-        )
+            random_state=self.random_state, )
         X_ = self.transformer_.fit_transform(X)
         self.dt_.fit(X_, y, **kwargs)
         return self
@@ -198,29 +204,29 @@ class CoordinateChangingDecisionTree(BaseEstimator):
         return self.dt_._validate_X_predict(X, check_input)
 
 
-class CoordinateChangingDecisionTreeClassifier(
-        CoordinateChangingDecisionTree,
-        ClassifierMixin):
+class CoordinateChangingDecisionTreeClassifier(CoordinateChangingDecisionTree,
+                                               ClassifierMixin):
     """
     A decision tree classifier which performs a random change of
     coordinates before fitting or predicting.
-    
-    The parameters are mostly the same as sklearn's DecisionTreeClassifier, with
-    the exception of transform_dimension, transform_repeats, and usecols, for
-    which see RandomCoordinateChanger.
+
+    The parameters are mostly the same as sklearn's DecisionTreeClassifier,
+    with the exception of transform_dimension, transform_repeats, and usecols,
+    for which see RandomCoordinateChanger.
 
     It's probably pointless to use this directly: it's useful as part of an
     ensemble.
     """
+
     def initiate_tree_type(self):
         self.tree_type_ = tree.DecisionTreeClassifier
 
 
-class CoordinateChangingDecisionTreeRegressor(
-        CoordinateChangingDecisionTree):
+class CoordinateChangingDecisionTreeRegressor(CoordinateChangingDecisionTree):
     """
     Not ready for prime time.
     """
+
     def initiate_tree_type(self):
         self.tree_type_ = tree.DecisionTreeRegressor
 
@@ -229,11 +235,12 @@ class RandomCoordinateForestClassifier(ensemble.forest.ForestClassifier):
     """
     A random forest for which each tree performs a random change of coordinates
     before fitting or predicting.
-    
+
     The parameters are largely the same as sklearn's RandomForestClassifier,
     with the exception of transform_dimension, transform_repeats, and usecols,
     for which see RandomCoordinateChanger.
     """
+
     def __init__(self,
                  transform_repeats='auto',
                  transform_dimension=None,
@@ -257,11 +264,11 @@ class RandomCoordinateForestClassifier(ensemble.forest.ForestClassifier):
         super(RandomCoordinateForestClassifier, self).__init__(
             base_estimator=CoordinateChangingDecisionTreeClassifier(),
             n_estimators=n_estimators,
-            estimator_params=("criterion", "max_depth", "min_samples_split",
-                              "min_samples_leaf", "min_weight_fraction_leaf",
-                              "max_features", "max_leaf_nodes", "min_impurity_split",
-                              "transform_repeats", "transform_dimension",
-                              "usecols", "random_state"),
+            estimator_params=(
+                "criterion", "max_depth", "min_samples_split",
+                "min_samples_leaf", "min_weight_fraction_leaf", "max_features",
+                "max_leaf_nodes", "min_impurity_split", "transform_repeats",
+                "transform_dimension", "usecols", "random_state"),
             bootstrap=bootstrap,
             oob_score=oob_score,
             n_jobs=n_jobs,
